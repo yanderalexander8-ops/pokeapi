719a4c1ceb82cde15e83ad71a7a804cb3d8a2b90
Yander <yanderalexander8@gmail.com>
2026-07-02 23:49:55 -0500

    Agrega modulo raiz con HttpClientModule registrado

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+
+@NgModule({
+  declarations: [AppComponent, PokemonListComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
